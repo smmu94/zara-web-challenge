2e969d634eea5e76ca67855486e94a821a33b320
smmu94 <stephaniemmu@gmail.com>
2025-02-01 21:29:45 +0100

    Added config for dev and prod enviroments

--- next.config.js
+++ next.config.js
@@ -8,11 +8,23 @@ const nextConfig = {
   images: {
     domains: ["prueba-tecnica-api-tienda-moviles.onrender.com"]
   },
-  webpack(config) {
+
+  webpack(config, { isServer }) {
     config.resolve.alias = {
       ...config.resolve.alias,
       "@styles": path.resolve(__dirname, "styles"),
     };
+
+    if (process.env.NEXT_PUBLIC_APP_ENV === "production") {
+      config.optimization.minimize = true;
+      config.optimization.splitChunks = {
+        chunks: 'all',
+      };
+    } else {
+      config.optimization.minimize = false;
+      config.optimization.splitChunks = false;
+    }
+
     return config;
   },
 };
