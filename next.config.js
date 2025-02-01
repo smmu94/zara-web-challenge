13a315c26115e99080610a6c198e72f8f5e530d2
smmu94 <stephaniemmu@gmail.com>
2025-02-01 17:20:41 +0100

    Added styles variables of typography, colors, borders and spacings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /* eslint-disable no-undef */
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['prueba-tecnica-api-tienda-moviles.onrender.com']
+    domains: ["prueba-tecnica-api-tienda-moviles.onrender.com"]
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@styles": path.resolve(__dirname, "styles"),
+    };
+    return config;
   },
 };
 
